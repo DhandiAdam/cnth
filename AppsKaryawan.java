7f3ae7b023ca2bce3c0f0044014db6f8130136ab
DhandiAdam <148371521+DhandiAdam@users.noreply.github.com>
2023-10-23 12:25:21 +0700

    APPSKARYAWAN

---
+++ AppsKaryawan.java
@@ -0,0 +1,11 @@
+public class AppsKaryawan {
+    public static void main(String[] args) {
+        Karyawan1 wahyu = new Karyawan1("Wahyu");
+        Dosen1 amir = new Dosen1("Amir Murtako");
+        Staff melan = new Staff("Melan");
+
+        wahyu.kerja();
+        amir.kerja();
+        melan.kerja();
+    }
+}
